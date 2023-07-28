14b68d3416c3956b4a226ca81d31a59c7dd6a896
Cristian Rossi <cristian.rossicosta@gmail.com>
2023-07-28 17:07:39 -0300

    Desgin v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
-import './App.css';
+import "./App.css";
 
-function App() {
+const tasks = [
+  {
+    id: 1,
+    name: "Estudar para prova",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    done: false,
+  },
+  {
+    id: 2,
+    name: "Dormir",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    done: true,
+  },
+];
+
+const AddTask = () => {
+  return (
+    <section className="section-add-task">
+      <input type="text" name="task-input" id="task-input" />
+      <Button value="Adicionar" />
+    </section>
+  );
+};
+
+const Button = ({ value }) => {
   return (
     <>
-    <h1>Task list</h1>
+      <button>{value}</button>
     </>
   );
-}
+};
 
-export default App;
+const ListTasks = ({ tasks }) => {
+  const taskList = tasks.map((task) => <Task key={task.id} task={task} />);
+
+  return <section className="section-list-tasks">{taskList}</section>;
+};
+
+const Task = ({ task }) => {
+  return (
+    <div className="task">
+      <p>{task.name}</p>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <main>
+      <h1>Minhas Tarefas</h1>
+      <AddTask />
+      <ListTasks tasks={tasks} />
+    </main>
+  );
+}
